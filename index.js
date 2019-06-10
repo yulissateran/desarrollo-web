903a777a0552ecca259f1d8cd32f84d1c6f13e18
yulissateran <yulissa.lteran@gmail.com>
2019-06-09 21:14:11 -0500

    crea estructura HTML , agrega estilos

---
+++ index.js
@@ -0,0 +1,19 @@
+
+/*
+
+DOM es el acrónimo de Document Object Model o Modelo de documento, y es la manera en que se representa el contenido del documento, de manera similar a un árbol de nodos.
+
+A continuación, un ejemplo sencillo de la estructura del DOM:
+
+html
+ head
+   title
+   meta
+ body
+   header
+    nav
+    section
+    article
+    footer
+
+*/
